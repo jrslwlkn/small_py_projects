b73f9f3a549e35ec20297ea6af36f7fa96b9ad2a
Yarek <jrslwlkn@gmail.com>
2020-03-21 18:26:19 -0500

    removed duplicates

--- palindromes/palindromes.py
+++ palindromes/palindromes.py
@@ -1,11 +1,10 @@
 import words_loader
 
-
 words = words_loader.load('words.txt')
 palindromes = []
 
 for word in words:
     if len(word) > 1 and word == word[::-1]:
         palindromes.append(word)
-        
+
 print(*palindromes, sep='\n')
